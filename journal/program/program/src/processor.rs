5d314795f553ff799e0132e5d6c7422de46d01f4
ngundotra <noah@gundotra.org>
2022-09-14 14:56:37 -0400

    journal: simplify processor logic

--- journal/program/program/src/processor.rs
+++ journal/program/program/src/processor.rs
@@ -1,32 +1,19 @@
 use borsh::BorshDeserialize;
 use solana_program::{
-    account_info::AccountInfo, 
-    entrypoint::ProgramResult, 
-    program_error::ProgramError,
+    account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
     pubkey::Pubkey,
 };
 
-use crate::instructions::JournalInstruction;
-use crate::instructions::JournalInstructionType;
-
+use crate::instructions::{init_journal, new_entry, JournalInstruction};
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-
-    match JournalInstruction::unpack(&instruction_data) {
-        JournalInstructionType::InitJournal(obj) => init_journal(
-            program_id,
-            accounts,
-            obj,
-        ),
-        JournalInstructionType::NewEntry(obj) => new_entry(
-            program_id,
-            accounts,
-            obj,
-        ),
+    match JournalInstruction::try_from_slice(&instruction_data)? {
+        JournalInstruction::InitJournal(args) => init_journal(program_id, accounts, args),
+        JournalInstruction::NewEntry(args) => new_entry(program_id, accounts, args),
         _ => Err(ProgramError::InvalidInstructionData),
     }
 }
