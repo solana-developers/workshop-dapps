da0d0a3083c2406423a0c0a2a0d92500ee9202bd
jpcaulfi <joe.caulfield@solana.org>
2022-09-21 14:48:01 +0800

    css

--- solana-twitter/app_v2/src/views/home/index.tsx
+++ solana-twitter/app_v2/src/views/home/index.tsx
@@ -9,8 +9,8 @@ export const HomeView: FC = ({ }) => {
 
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        <h1 className="mt-4 text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#74a8fc] to-[#c6ecf7]">
-          Solana Twitter <span className='text-sm font-normal align-top text-slate-700'>v{pkg.version}</span>
+        <h1 className="mt-2 text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#74a8fc] to-[#c6ecf7]">
+          Solana Twitter
         </h1>
         <SolanaTwitter />
       </div>
