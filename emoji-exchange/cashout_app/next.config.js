b3bbd84606ff183e80f994ae7626a771121ccf04
jpcaulfi <joe.caulfield@solana.org>
2022-10-14 09:26:17 -0500

    removed cashout_app from emoji

--- emoji-exchange/cashout_app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    RPC_ENDPOINT: process.env.RPC_ENDPOINT,
-  }
-}
-
-module.exports = nextConfig
